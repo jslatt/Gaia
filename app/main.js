0ec4f0d88891d2e25e5fad7058829f345317b72e
_lucidEntropy <james@lucidentropy.ooo>
2023-08-08 07:44:34 -0400

    Formatting

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 const { Configuration, OpenAIApi } = require("openai");
-
+const parse = require('html-react-parser');
 
 export default function Main() {
 
@@ -20,9 +20,6 @@ export default function Main() {
     };
     const changeLong = (event) => {
         setLong(event.target.value);
-    };
-    const changeDate = (event) => {
-        setDate(event.target.value);
     };
 
 
@@ -57,7 +54,7 @@ export default function Main() {
     function getWeather() {
         axios.request(options).then(function (response) {
             setData(response.data);
-            let p = "Write a Marine Corps analysis with the following location and weather data. Please provide specific guidance as to how this weather can impact the Marines willingness to fight, and how the USMC can use this as an advantage against the enemy.  Provide it in the specific format: Impact of Weather on Marine's Willingness to Fight, Positive Impacts, Negative Impacts. Do not provide any other information.\n " + JSON.stringify(response.data);
+            let p = "Write a Marine Corps analysis with the following location and weather data. Please provide specific guidance as to how this weather can impact the Marines willingness to fight, and how the USMC can use this as an advantage against the enemy.  Provide it in the specific format: Impact of Weather on Marine's Willingness to Fight, Positive Impacts, Negative Impacts. Do not provide any other information. Split it up by each section and format the output in an HTML Div. Do not include <head> tags or <HTML> tags. \n " + JSON.stringify(response.data);
             p = p.substring(0, 8000)
             console.log(p);
             askGPT(p)
@@ -81,9 +78,8 @@ export default function Main() {
                         <div className="relative">
                             <input type="text" onChange={changeLat} className="block w-full p-4 pl-10 text-sm text-gray-900  bg-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="LATTITUDE" required />
                             <input type="text" onChange={changeLong} className="block w-full p-4 pl-10 text-sm text-gray-900  bg-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="LONGITUDE" required />
-                            <input type="date" onChange={changeDate} className="block w-full p-4 pl-10 text-sm text-gray-900  bg-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Date" required />
 
-                            <a onClick={getWeather} className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Analyze Weather</a>
+                            <a onClick={getWeather} className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Analyze</a>
                         </div>
                     </form>
 
@@ -96,12 +92,12 @@ export default function Main() {
                                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                                 <path d="M22 4L12 14.01l-3-3"></path>
                             </svg>
-                            <span className="title-font font-medium text-white">Lat: {data ? JSON.stringify(data.location.lat) : null}, Long: {data ? JSON.stringify(data.location.lon) : null}</span>
+                            <span className="title-font font-medium text-white">{data ? "Lat: " + JSON.stringify(data.location.lat) : "Enter a location to begin..."}{data ? ", Long: " + JSON.stringify(data.location.lon) : null}</span>
                         </div>
                     </div>
                     <h2 className="sm:text-3xl text-2xl text-white title-font font-medium text-gray-900 mt-4 mb-4">Weather Warfighting Analysis</h2>
                     <span className="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">OpenAI GPT-3.5</span>
-                    <p className='py-4'>{analysis}</p>
+                    <div className='py-4'>{parse(analysis)}</div>
 
 
                 </div>
