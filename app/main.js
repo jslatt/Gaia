318c71e0312ee228a2c99e240d8174a91f46d427
_lucidEntropy <james@lucidentropy.ooo>
2023-08-08 08:29:03 -0400

    Maps, Loading Spinner

--- app/main.js
+++ app/main.js
@@ -1,17 +1,18 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 const { Configuration, OpenAIApi } = require("openai");
 const parse = require('html-react-parser');
+import { Loader } from '@googlemaps/js-api-loader';
 
 export default function Main() {
 
     //const [image, setImage] = useState(null);
-    const [lat, setLat] = useState(null);
-    const [long, setLong] = useState(null);
-    const [date, setDate] = useState(null);
+    const [lat, setLat] = useState(38.5167915);
+    const [long, setLong] = useState(-77.2988706);
     const [data, setData] = useState('');
-    const [analysis, setAnalysis] = useState('');
+    const [analysis, setAnalysis] = useState(' ');
+    const [map, setMap] = useState(null);
 
 
 
@@ -51,18 +52,59 @@ export default function Main() {
         setAnalysis(completion.data.choices[0].message.content);
     }
 
+        // GOOGLE MAP
+        const loadMap = async () => {
+            const loader = new Loader({
+              apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+              libraries: ['places'],
+            });
+            const google = await loader.load();
+        
+            const mapOptions = {
+              center: { lat: parseFloat(lat), lng: parseFloat(long) },
+              zoom: 12,
+            };
+        
+            setMap(new google.maps.Map(document.getElementById('map'), mapOptions));
+          };
+        
+          useEffect(() => {
+            loadMap();
+          }, []);
+
+          
+
     function getWeather() {
+
+        setAnalysis('');
+
         axios.request(options).then(function (response) {
             setData(response.data);
-            let p = "Write a Marine Corps analysis with the following location and weather data. Please provide specific guidance as to how this weather can impact the Marines willingness to fight, and how the USMC can use this as an advantage against the enemy.  Provide it in the specific format: Impact of Weather on Marine's Willingness to Fight, Positive Impacts, Negative Impacts. Do not provide any other information. Split it up by each section and format the output in an HTML Div. Do not include <head> tags or <HTML> tags. \n " + JSON.stringify(response.data);
+            let p = "Write a Marine Corps analysis with the following location and weather data. Please provide specific guidance as to how this weather can impact the Marines willingness to fight, and how the USMC can use this as an advantage against the enemy.  Provide it in the specific format: Impact of Weather on Marine's Willingness to Fight (h1), Positive Impacts (h2), Negative Impacts (h2). Do not provide any other information. It should be at least 500 words. Split it up by each section and format the output in an HTML Div. Do not include <head> tags or <HTML> tags. \n " + JSON.stringify(response.data);
             p = p.substring(0, 8000)
             console.log(p);
             askGPT(p)
         }).catch(function (error) {
             console.error(error);
         });
-    }
 
+        const map = setMap(new google.maps.Map(document.getElementById('map'), {
+            center: { lat: parseFloat(lat), lng: parseFloat(long) },
+            zoom: 12,
+          }));
+    }
+    
+    const spinner = `
+    <center>
+    <div role="status">
+    <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
+    </div>
+    </center>
+`
 
 
     return (
@@ -83,12 +125,14 @@ export default function Main() {
                         </div>
                     </form>
 
-
+                    <div>
+                    <div id="map" style={{ height: '600px' }}></div>
+                    </div>
                 </div>
                 <div className="bg-grey-900 h-screen p-4 col-span-3">
                     <div className="p-2  w-full">
                         <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                                 <path d="M22 4L12 14.01l-3-3"></path>
                             </svg>
@@ -97,7 +141,7 @@ export default function Main() {
                     </div>
                     <h2 className="sm:text-3xl text-2xl text-white title-font font-medium text-gray-900 mt-4 mb-4">Weather Warfighting Analysis</h2>
                     <span className="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">OpenAI GPT-3.5</span>
-                    <div className='py-4'>{parse(analysis)}</div>
+                    <div className='py-4'>{analysis ? parse(analysis) : parse(spinner)}</div>
 
 
                 </div>
--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     reactStrictMode: true,
   env: {
     TMRW_IO: process.env.TMRW_IO,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    REACT_APP_GOOGLE_MAPS_API_KEY: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   }
 }
 
